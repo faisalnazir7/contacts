6ab25dc17b50d1a86c352f31cf2b57e5fd61382f
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-09-11 01:58:06 +0530

    contactModel

---
+++ Server/src/models/contactModel.js
@@ -0,0 +1,28 @@
+// models/contactModel.js
+const mongoose = require('mongoose');
+
+// Define the Contact schema
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // Ensures email is unique
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create the Contact model
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
