b5991fcf7f1fd8952e9030db5963e8a3a6406a21
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-09-12 03:09:59 +0530

    CORS:origin added

--- Server/src/server.js
+++ Server/src/server.js
@@ -24,6 +24,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      "https://contacts-beige.vercel.app",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
